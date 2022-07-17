e70d12c253e9b4ffdd53ce3d56dad04b2636f273
Roshini Rangarajan <53918739+roshinira123@users.noreply.github.com>
2022-07-17 03:22:37 +0000

    Datatypes Example 1

---
+++ .ccls-cache/@home@runner@CPP1/datatypes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+//Addition 
+// Example 1
+//Do not have to use std: :endl
+int apples {10};
+int oranges {20};
+int total_fruit {apples+oranges}; 
+
+std::cout << "The value of apple is " << apples << "\n";
+std::cout << "The value of oranges is " << oranges << "\n";
+std::cout << "The value of all fruit is " << total_fruit<< "\n";
+
+  }
--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
-
+//First program 
+//Commented out programs that are not being worked on 
+/*
 int main() {
-  std::cout << "Hello World!\n";
+  std::cout << "Hello World!";
   std::cout <<"Roshini was here!";
+  
 }
+*/
